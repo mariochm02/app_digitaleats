fbf378302fcd423010ab5f5539f40a3e57f9dac0
mariochm02 <Mario.chulian@iesdonana.org>
2024-06-10 11:47:56 +0000

    cambios css

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,16 +3,15 @@ import { Link } from '@inertiajs/react';
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <main className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-black"> 
+            <header className='w-30 h-30'>
+            <img className='w-80 h-80' src="https://live.staticflickr.com/65535/53778664629_1be5640ba5_m.jpg" alt="" />
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
-            </div>
-        </div>
+            </header>
+
+            <article className="w-full bg-black sm:max-w-md mt-1 px-6 py-4 text-white shadow-md overflow-hidden sm:rounded-lg border-amber-300 border-4">
+                {children} 
+            </article>
+        </main>
     );
 }
